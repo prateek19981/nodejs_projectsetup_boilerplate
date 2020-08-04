2c591002444f052e06fd9da2647f8ac5f86f1a17
prateek <gprateek558@gmail.com>
2020-08-04 11:18:16 -0700

    project setup

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = 8000;
 const app = express();
 //fEYu6G7VZXEeElgi
 
+app.use(express.json());
+app.use(require("./routes"));
 app.listen(port, (err) => {
   if (err) {
     console.log("err in connecting to server");
